cf4a12ded152b7e0b34fa5fb8b21690c82b63cb3
Shubham <shubham86.y@gmail.com>
2026-06-25 21:10:02 +0530

    Initial commit

---
+++ inputoutput.cpp
@@ -0,0 +1,8 @@
+//Take an integer as input and display it.
+#include<iostream>
+int main(){
+    int number;
+    std::cin>>number;
+    std::cout<<"Number you entered was:"<<number<<std::endl;
+    return 0;
+}
