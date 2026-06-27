b76bee005dee37c6b3fb7b3422f7932b5bf8a330
Shubham <shubham86.y@gmail.com>
2026-06-27 20:44:15 +0530

    Clean up main branch

--- inputoutput.cpp
+++
@@ -1,8 +0,0 @@
-//Take an integer as input and display it.
-#include<iostream>
-int main(){
-    int number;
-    std::cin>>number;
-    std::cout<<"Number you entered was:"<<number<<std::endl;
-    return 0;
-}
